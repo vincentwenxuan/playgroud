48240acfe7497bcc45a71618e53ec57c866eedb0
vincentwenxuan <vincentwenxuan@gmail.com>
2015-12-27 18:05:04 -0500

    split check event to be maintainable

--- Python_Crash_Course/py_alien_invasion/game_functions.py
+++ Python_Crash_Course/py_alien_invasion/game_functions.py
@@ -9,23 +9,33 @@ def check_event(player_ship):
         if event.type == pygame.QUIT:
             sys.exit()
         elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_RIGHT:
-                player_ship.moving_right = True
-            elif event.key == pygame.K_LEFT:
-                player_ship.moving_left = True
-            elif event.key == pygame.K_UP:
-                player_ship.moving_up = True
-            elif event.key == pygame.K_DOWN:
-                player_ship.moving_down = True
+            check_keydown_event(event,player_ship)
         elif event.type == pygame.KEYUP:  
-            if event.key == pygame.K_RIGHT:
-                player_ship.moving_right = False
-            if event.key == pygame.K_LEFT:
-                player_ship.moving_left = False
-            if event.key == pygame.K_UP:
-                player_ship.moving_up = False
-            if event.key == pygame.K_DOWN:
-                player_ship.moving_down = False
+            check_keyup_event(event,player_ship)
+
+def check_keydown_event(event,player_ship):
+    '''check when key pressed'''
+    if event.key == pygame.K_RIGHT:
+        player_ship.moving_right = True
+    elif event.key == pygame.K_LEFT:
+        player_ship.moving_left = True
+    elif event.key == pygame.K_UP:
+        player_ship.moving_up = True
+    elif event.key == pygame.K_DOWN:
+        player_ship.moving_down = True    
+
+def check_keyup_event(event,player_ship):
+    '''check when key released'''
+    if event.key == pygame.K_RIGHT:
+        player_ship.moving_right = False
+    elif event.key == pygame.K_LEFT:
+        player_ship.moving_left = False
+    elif event.key == pygame.K_UP:
+        player_ship.moving_up = False
+    elif event.key == pygame.K_DOWN:
+        player_ship.moving_down = False
+
+
 def update_screen(ai_settings, screen, player_ship):
     ''' update all elements in screen according to settings '''
 
--- Python_Crash_Course/py_alien_invasion/settings.py
+++ Python_Crash_Course/py_alien_invasion/settings.py
@@ -4,7 +4,13 @@
 class Settings(object):
     """Settings for the alien invasion game"""
     def __init__(self):
+        
+        # general settings
         self.screen_size = (1200,800)
         self.bg_color = (40,40,40)
+
+        # player ship settings
         self.player_speed = (1.5,0.5)
+        
+        # bullet settings
         
